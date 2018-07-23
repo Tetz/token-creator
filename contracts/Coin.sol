c3b7ef308d8e998baed9c44e5c446b13bed01035
Tetsuro Takemoto <t@ineeza.com>
2018-07-23 14:42:16 +0900

    chore: Add deploy script

--- contracts/Coin.sol
+++ contracts/Coin.sol
@@ -2,9 +2,9 @@ pragma solidity ^0.4.18;
 import "../node_modules/zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
 contract Coin is StandardToken {
-  string public name = "Quick Pass Token for popular Tokyo Sushi restaurant. Spot number: 1";
-  string public symbol = "QPT-1";
-  uint public decimals = 2;
+  string public name = "Test Coin";
+  string public symbol = "TSC";
+  uint public decimals = 18;
   uint public INITIAL_SUPPLY = 100000000 * (10 ** decimals);
 
   function Coin() public {
